95092c2428c88e162799ee4ab2df9c4f050081cf
Leandro Borges Ferreira <lehen01@gmail.com>
2021-03-15 16:00:24 -0300

    [CAS-588] Test - correct request is made to DAO

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/MessageRepositoryTests.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/MessageRepositoryTests.kt
@@ -1,19 +1,25 @@
 package io.getstream.chat.android.livedata.repository
 
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.inOrder
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
+import io.getstream.chat.android.client.api.models.Pagination
 import io.getstream.chat.android.livedata.randomMessage
 import io.getstream.chat.android.livedata.randomMessageEntity
 import io.getstream.chat.android.livedata.randomUser
 import io.getstream.chat.android.livedata.repository.domain.message.MessageDao
 import io.getstream.chat.android.livedata.repository.domain.message.MessageRepository
 import io.getstream.chat.android.livedata.repository.domain.message.MessageRepositoryImpl
+import io.getstream.chat.android.livedata.request.AnyChannelPaginationRequest
+import io.getstream.chat.android.test.randomString
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.Date
 
 @ExperimentalCoroutinesApi
 internal class MessageRepositoryTests {
@@ -44,5 +50,50 @@ internal class MessageRepositoryTests {
         result.any { it.id == "id2" } shouldBeEqualTo true
         result.any { it.id == "id3" } shouldBeEqualTo true
         result.any { it.id == "id4" } shouldBeEqualTo true
+    }
+
+    @Test
+    fun `when selecting messages for channel, correct messages should be requested to DAO`() = runBlockingTest {
+        val createdAt = Date()
+        val cid = randomString()
+        val messageEntity = randomMessageEntity(createdAt = createdAt)
+
+        val requestGreaterOrEqual = AnyChannelPaginationRequest(30).apply {
+            messageFilterDirection = Pagination.GREATER_THAN_OR_EQUAL
+        }
+
+        val requestGreater = AnyChannelPaginationRequest(30).apply {
+            messageFilterDirection = Pagination.GREATER_THAN
+        }
+        
+        val requestLessThan = AnyChannelPaginationRequest(30).apply {
+            messageFilterDirection = Pagination.LESS_THAN
+        }
+
+        val requestLessOrEqualThan = AnyChannelPaginationRequest(30).apply {
+            messageFilterDirection = Pagination.LESS_THAN_OR_EQUAL
+        }
+
+        whenever(messageDao.select(requestGreaterOrEqual.messageFilterValue)) doReturn messageEntity
+        whenever(messageDao.select(requestGreater.messageFilterValue)) doReturn messageEntity
+        whenever(messageDao.select(requestLessThan.messageFilterValue)) doReturn messageEntity
+        whenever(messageDao.select(requestLessOrEqualThan.messageFilterValue)) doReturn messageEntity
+
+        whenever(messageDao.messagesForChannelEqualOrNewerThan(any(), any(), any())) doReturn listOf(messageEntity)
+        whenever(messageDao.messagesForChannelNewerThan(any(), any(), any())) doReturn listOf(messageEntity)
+        whenever(messageDao.messagesForChannelEqualOrOlderThan(any(), any(), any())) doReturn listOf(messageEntity)
+        whenever(messageDao.messagesForChannelOlderThan(any(), any(), any())) doReturn listOf(messageEntity)
+
+        sut.selectMessagesForChannel(cid, requestGreaterOrEqual)
+        sut.selectMessagesForChannel(cid, requestGreater)
+        sut.selectMessagesForChannel(cid, requestLessThan)
+        sut.selectMessagesForChannel(cid, requestLessOrEqualThan)
+
+        inOrder(messageDao).run {
+            verify(messageDao).messagesForChannelEqualOrNewerThan(cid, requestGreaterOrEqual.messageLimit, createdAt)
+            verify(messageDao).messagesForChannelNewerThan(cid, requestGreaterOrEqual.messageLimit, createdAt)
+            verify(messageDao).messagesForChannelOlderThan(cid, requestLessOrEqualThan.messageLimit, createdAt)
+            verify(messageDao).messagesForChannelEqualOrOlderThan(cid, requestLessThan.messageLimit, createdAt)
+        }
     }
 }
