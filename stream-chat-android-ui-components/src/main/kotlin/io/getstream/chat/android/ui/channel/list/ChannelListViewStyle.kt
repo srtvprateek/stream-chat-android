1a26d73a4a38ac7476d8636209bea843d57de90f
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-03-19 14:05:02 +0100

    Open `ChannelListViewStyle`

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/ChannelListViewStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/ChannelListViewStyle.kt
@@ -4,41 +4,42 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.annotation.Px
 import io.getstream.chat.android.ui.R
-import io.getstream.chat.android.ui.common.extensions.internal.use
 
-public class ChannelListViewStyle internal constructor(context: Context, attrs: AttributeSet?) {
-    @Px
-    public val channelTitleTextSize: Float
+public data class ChannelListViewStyle(
+    @Px public val channelTitleTextSize: Float,
+    @Px public val lastMessageSize: Float,
+    @Px public val lastMessageDateTextSize: Float,
+) {
 
-    @Px
-    public val lastMessageSize: Float
+    internal companion object {
+        operator fun invoke(context: Context, attrs: AttributeSet?): ChannelListViewStyle =
+            context.obtainStyledAttributes(
+                attrs,
+                R.styleable.ChannelListView,
+                0,
+                0
+            ).let { a ->
+                val resources = context.resources
 
-    @Px
-    public val lastMessageDateTextSize: Float
+                val channelTitleTextSize = a.getDimensionPixelSize(
+                    R.styleable.ChannelListView_streamUiChannelTitleTextSize,
+                    resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_title)
+                ).toFloat()
 
-    init {
-        context.obtainStyledAttributes(
-            attrs,
-            R.styleable.ChannelListView,
-            0,
-            0
-        ).use { a ->
-            val resources = context.resources
+                val lastMessageSize = a.getDimensionPixelSize(
+                    R.styleable.ChannelListView_streamUiLastMessageTextSize,
+                    resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_message)
+                ).toFloat()
 
-            channelTitleTextSize = a.getDimensionPixelSize(
-                R.styleable.ChannelListView_streamUiChannelTitleTextSize,
-                resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_title)
-            ).toFloat()
-
-            lastMessageSize = a.getDimensionPixelSize(
-                R.styleable.ChannelListView_streamUiLastMessageTextSize,
-                resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_message)
-            ).toFloat()
-
-            lastMessageDateTextSize = a.getDimensionPixelSize(
-                R.styleable.ChannelListView_streamUiLastMessageDateTextSize,
-                resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_message_date)
-            ).toFloat()
-        }
+                val lastMessageDateTextSize = a.getDimensionPixelSize(
+                    R.styleable.ChannelListView_streamUiLastMessageDateTextSize,
+                    resources.getDimensionPixelSize(R.dimen.stream_ui_channel_item_message_date)
+                ).toFloat()
+                ChannelListViewStyle(
+                    channelTitleTextSize = channelTitleTextSize,
+                    lastMessageSize = lastMessageSize,
+                    lastMessageDateTextSize = lastMessageDateTextSize,
+                )
+            }
     }
 }
